6b8817333815d4b95d936a8a67ba81d4deb811b8
Mohammed Armaan <armaan0243@gmail.com>
2023-12-23 02:30:45 +0530

    log sig

---
+++ src/pages/Login.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import * as Components from '../components/Componentst';
+
+function App() {
+    const [signIn, toggle] = React.useState(true);
+     return(
+      <>
+      <center>
+         <Components.Container>
+             <Components.SignUpContainer signinIn={signIn}>
+                 <Components.Form>
+                 <Components.Title>Teacher</Components.Title>
+                      <Components.Input type='email' placeholder='Teacher Id' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                      <Components.Button>Sigin In</Components.Button>
+                 </Components.Form>
+             </Components.SignUpContainer>
+
+             <Components.SignInContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Student</Components.Title>
+                      <Components.Input type='email' placeholder='USN' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                      <Components.Button>Sigin In</Components.Button>
+                      
+                  </Components.Form>
+             </Components.SignInContainer>
+
+             <Components.OverlayContainer signinIn={signIn}>
+                 <Components.Overlay signinIn={signIn}>
+
+                 <Components.LeftOverlayPanel signinIn={signIn}>
+                     <Components.Title>For Students</Components.Title>
+                     <Components.Paragraph>
+                         Students login here
+                     </Components.Paragraph>
+                     <Components.GhostButton onClick={() => toggle(true)}>
+                         Sign In
+                     </Components.GhostButton>
+                     </Components.LeftOverlayPanel>
+
+                     <Components.RightOverlayPanel signinIn={signIn}>
+                       <Components.Title>For Teachers</Components.Title>
+                       <Components.Paragraph>
+                           Teachers login here
+                       </Components.Paragraph>
+                           <Components.GhostButton onClick={() => toggle(false)}>
+                              Sign In
+                           </Components.GhostButton> 
+                     </Components.RightOverlayPanel>
+ 
+                 </Components.Overlay>
+             </Components.OverlayContainer>
+
+         </Components.Container>
+         </center>
+         </>
+     )
+}
+
+export default App;
