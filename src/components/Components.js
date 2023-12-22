7098b27b27f961290073219827c77f2324a0dd5c
Mohammed Armaan <armaan0243@gmail.com>
2023-12-23 04:05:11 +0530

    hi

--- src/components/Components.js
+++ src/components/Components.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
 margin-top: 100px;
 background-color: #fff;
+
 border-radius: 10px;
 box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 position: relative;
