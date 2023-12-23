e0e66a4a116d0878abfe376e1ebf6e0b54e779f6
Mohammed Armaan <armaan0243@gmail.com>
2023-12-23 19:20:48 +0530

    nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import { Link, useNavigate } from 'react-router-dom';
 export default function Home() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar bg="dark" variant="dark" expand="lg" className='sticky-top'>
         <Navbar.Brand as={Link} to="/" className="ml-auto"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
